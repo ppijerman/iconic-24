92eb2b0836cba0b2bd15065da5fa6dfd61c16574
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-03-29 12:08:31 +0100

    asdad

---
+++ app/nyobain/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-3xl md:text-5xl font-bold">Our nyobain</h1>
+      <h1 className="text-3xl md:text-5xl font-bold">TBA April 2024</h1>
+    </main>
+  );
+}
