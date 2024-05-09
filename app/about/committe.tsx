1a62129b9ce80eaa23e5c0120ccb89347b440653
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-05-09 23:10:25 +0200

    feat: add committee photos

---
+++ app/about/committe.tsx
@@ -0,0 +1,35 @@
+import agnia from "@/assets/commitee/Agnia.jpeg";
+import sekar from "@/assets/commitee/sekar.jpeg";
+import gamma from "@/assets/commitee/gamma.jpeg";
+import geraldus from "@/assets/commitee/geraldus.jpeg";
+import caryn from "@/assets/commitee/caryn.jpg";
+import azhar from "@/assets/commitee/azhar.jpeg";
+import gerraldo from "@/assets/commitee/Gerraldo.jpeg";
+import imel from "@/assets/commitee/imel.png";
+import chaska from "@/assets/commitee/Chaska.jpg";
+import hessel from "@/assets/commitee/hessel.jpg";
+import rohmaneo from "@/assets/commitee/rohmaneo.jpg";
+import eunike from "@/assets/commitee/eunike.png";
+import hosea from "@/assets/commitee/hosea.jpg";
+import marcelian from "@/assets/commitee/marcelian.jpg";
+import hapsari from "@/assets/commitee/hapsari.jpg";
+import fadlilah from "@/assets/commitee/fadlilah.jpg";
+import bea from "@/assets/commitee/bea.jpg";
+import santi from "@/assets/commitee/santi.jpg";
+import kinasih from "@/assets/commitee/kinasih.jpg";
+import evelyn from "@/assets/commitee/evelyn.jpg";
+import syifa from "@/assets/commitee/syifa.jpg";
+import taqi from "@/assets/commitee/taqi.jpg";
+import nata from "@/assets/commitee/nata.jpg";
+import maulana from "@/assets/commitee/maulana.jpg";
+import tasa from "@/assets/commitee/tasallia.jpg";
+import aditya from "@/assets/commitee/aditya.jpg";
+import kafa from "@/assets/commitee/kafa.jpg";
+import tazya from "@/assets/commitee/tazya.jpg";
+import christine from "@/assets/commitee/christine.jpg";
+import risma from "@/assets/commitee/risma.jpeg";
+import kemal from "@/assets/commitee/kemal.jpg";
+
+export const committe = [
+    
+];
