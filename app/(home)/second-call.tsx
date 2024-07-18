05e809c97d8d6e3f5bfeddbfbbd4d4583cec2694
Azhar Rahadian <a.rahadian@tu-braunschweig.de>
2024-07-18 17:21:52 +0200

    added call to actions for ticket, and third call

--- app/(home)/second-call.tsx
+++ app/(home)/second-call.tsx
@@ -3,8 +3,8 @@ import { FileTextIcon } from "@radix-ui/react-icons";
 export function SecondCall() {
   return (
     <div className="flex flex-col items-center text-accent bg-gradient-to-r from-transparent via-white/70 to-transparent py-10 gap-3">
-      <h1 className="text-5xl font-bold">SECOND CALL FOR ABSTRACT</h1>
-      <h2 className="text-4xl font-semibold">May 15th - June 30th 2024</h2>
+      <h1 className="text-5xl font-bold">THIRD CALL FOR ABSTRACT</h1>
+      <h2 className="text-4xl font-semibold bg-gray-50 py-2 px-6 rounded-lg">July 15th - August 4th</h2>
       <div className="flex gap-2">
         <p className="text-xl"> Submit your abstract now!</p>
       </div>
