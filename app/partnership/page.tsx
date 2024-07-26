bb82c108a30d517531eb9c25362dbd765c2534a9
imeldaputr <imel-kein-email@gmail.com>
2024-07-26 20:13:48 +0200

    resolve reviews

--- app/partnership/page.tsx
+++ app/partnership/page.tsx
@@ -7,12 +7,18 @@ import dicoLogo from "@/assets/dico.png";
 import { Metadata } from "next";
 import { APP_NAME } from "@/lib/constants";
 
+import logoPPIJ from "@/assets/PPIJ_Logo_Color.png";
+import logoEcadin from "@/assets/sponsors/Knowledge_ECADIN.png";
+
 import logoCBL from "@/assets/sponsors/Platinum_CBL_NoBG.png";
 import logoUniGoe from "@/assets/sponsors/Platinum_University of Göttingen/GOE_Logo_Quer_IPC_Farbe_RGB.png";
-import logoDW from "@/assets/sponsors/Platinum_DW.svg";
 import logoKBRIBerlin from "@/assets/sponsors/Platinum_KBRI.jpeg";
 import logoTreeo from "@/assets/sponsors/Platinum_Treeo.png";
 import logoGIZ from "@/assets/sponsors/Platinum_GIZ/Platinum_giz.jpg";
+import logoAstra from "@/assets/sponsors/Platinum_Astra.png";
+import logoQSI from "@/assets/sponsors/Platinum_QSI.png";
+import logoKJRIFrankfurt from "@/assets/sponsors/Silver_KJRI.png";
+import logoZinkPower from "@/assets/sponsors/Silver_Zinkpower.png";
 
 import logoPPID from "@/assets/mediaPartner/PPID.png";
 import logoIFSA from "@/assets/mediaPartner/IFSA.svg";
@@ -30,30 +36,45 @@ export const metadata: Metadata = {
   description: "Contact us for partnership",
 };
 
-const sponsors = [
+const sponsorsPlatinum = [
   {
-    name: "CBL",
-    logo: logoCBL,
-  },
-  {
-    name: "University of Göttingen",
-    logo: logoUniGoe,
-  },
-  {
-    name: "Deutsche Welle",
-    logo: logoDW,
-  },
-  {
-    name: "KBRI Berlin",
-    logo: logoKBRIBerlin,
+    name: "GIZ",
+    logo: logoGIZ,
   },
   {
     name: "Treeo",
     logo: logoTreeo,
   },
   {
-    name: "GIZ",
-    logo: logoGIZ,
+    name: "Astra",
+    logo: logoAstra,
+  },
+  {
+    name: "KBRI Berlin",
+    logo: logoKBRIBerlin,
+  },
+  {
+    name: "University of Göttingen",
+    logo: logoUniGoe,
+  },
+  {
+    name: "CBL",
+    logo: logoCBL,
+  },
+  {
+    name: "QSI",
+    logo: logoQSI,
+  },
+];
+
+const sponsorsSilver = [
+  {
+    name: "KJRI Frankfurt",
+    logo: logoKJRIFrankfurt,
+  },
+  {
+    name: "Zink Power",
+    logo: logoZinkPower,
   },
 ];
 
@@ -102,7 +123,7 @@ const mediaPartners = [
 
 export default function Component() {
   return (
-    <main className="flex flex-col min-h-screen">
+    <main className="flex flex-col min-h-screen bg-white">
       <header className="bg-gray-100 dark:bg-gray-800 py-12 md:py-16 lg:py-20 w-full">
         <div className="">
           <div className="max-w-3xl mx-auto text-center space-y-4">
@@ -117,28 +138,83 @@ export default function Component() {
           </div>
         </div>
       </header>
-      <section className="py-12 md:py-16 lg:py-20">
-        <div className="container px-4 md:px-6">
-          <h2 className="text-4xl font-bold mb-8">Our Sponsors</h2>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 md:gap-8">
-            {sponsors.map((partner) => (
-              <div
-                key={partner.name}
-                className="flex justify-center items-center"
-              >
-                <Image
-                  src={partner.logo}
-                  alt={partner.name}
-                  width={200}
-                  height={200}
-                />
-              </div>
-            ))}
+
+      <div className="container flex flex-col gap-5 mt-10 lg:w-1/2 text-center justify-center items-center ">
+        <h2 className="text-4xl font-bold mt-8 text-center">Our Sponsors</h2>
+        <p className="text-gray-600 dark:text-gray-400">Becoming a sponsor for ICONIC 2024, a biennial international scientific conference in Germany by PPI Jerman, offers your company a prime opportunity to enhance its visibility on the international academic stage. ICONIC bridges Indonesia and Germany through discussions on sustainability and environmental awareness, engaging key decision-makers and industry leaders from academia, economics, and policymaking.</p>
+        <p className="text-gray-600 dark:text-gray-400">By supporting ICONIC 2024, you can effectively reach your target audience, build meaningful connections, and demonstrate your commitment to sustainable solutions. Seize this chance to connect, influence, and lead towards a more inclusive and sustainable future.</p>
+        <div className="flex sm:flex-row flex-col gap-5 mt-5">
+          <div className="sm:w-fit flex p-5 pe-0.5 justify-between sm:justify-center bg-zinc-400 rounded shadow-lg">
+            <div className="text-sm md:text-base text-white text-left font-bold">
+              <p>Hosted <br></br> by</p>              
+            </div>
+            <Image src={logoPPIJ} alt="Logo PPIJ" className="max-h-20 max-w-48"/>
+          </div>
+          <div className="sm:w-fit flex gap-5 p-5 justify-between sm:justify-center bg-zinc-400 rounded shadow-lg">
+            <div className="text-sm md:text-base text-white text-left font-bold ">
+              <p>Knowledge <br></br> Partner</p>
+            </div>
+            <Image src={logoEcadin} alt="Logo Ecadin" className="max-h-20 max-w-60"/>
           </div>
         </div>
+      </div>
+
+      <section className="container mt-5 pt-10 md:py-10 divide-y-2 divide-grey-200">
+        <div className="w-full pb-10 flex flex-col items-center gap-5">
+          <div className="flex items-center justify-center gap-5">
+            <Image width="50" height="50" src="https://img.icons8.com/ios/68/6b7280/hand-planting.png" alt="hand-planting" className="size-fit"/>
+            <h2 className="w-min sm:w-fit font-bold text-3xl text-center bg-clip-text text-transparent bg-gradient-to-r from-gray-400 via-gray-300 to-gray-400">PLATINUM SPONSOR</h2>
+          </div>
+          <div className="w-full flex justify-center flex-wrap gap-7">
+            {sponsorsPlatinum.map((partner) => (
+                <div
+                  key={partner.name}
+                  className="flex justify-center items-center"
+                >
+                  <Image
+                    src={partner.logo}
+                    alt={partner.name}
+                    width={150}
+                    height={150}
+                  />
+                </div>
+              ))}
+          </div>
+        </div> 
+        <div className="w-full pb-10 flex flex-col items-center gap-5">
+          <div className="flex items-center justify-center gap-5 mt-5">
+            <Image width="50" height="50" src="https://img.icons8.com/ios/68/F9C620/hand-planting.png" alt="hand-planting" className="size-fit"/>
+            <h2 className="font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-amber-400 via-amber-200 to-amber-400">GOLD SPONSOR</h2>
+          </div>
+          <div className="w-full flex justify-center flex-wrap gap-5 ">
+            <p className="text-2xl">TO BE ANNOUNCED</p>      
+          </div>
+        </div>
+        <div className="w-full pb-10 flex flex-col items-center gap-5">
+          <div className="flex items-center justify-center gap-5 mt-5">
+            <Image width="50" height="50" src="https://img.icons8.com/ios/68/A1A1AA/hand-planting.png" alt="hand-planting" className="size-fit"/>
+            <h2 className="font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-zinc-400 to-zinc-200">SILVER SPONSOR</h2>
+          </div>
+          <div className="w-full flex justify-center flex-wrap gap-10">
+            {sponsorsSilver.map((partner) => (
+                <div
+                  key={partner.name}
+                  className="flex justify-center items-center"
+                >
+                  <Image
+                    src={partner.logo}
+                    alt={partner.name}
+                    width={150}
+                    height={150}
+                  />
+                </div>
+              ))}
+          </div>
+        </div>
+        
       </section>
-      <section className="py-12 md:py-16 lg:py-20">
-        <div className="container px-4 md:px-6">
+      <section className="px-10">
+        <div className="container p-10 text-center shadow-2xl rounded-xl">
           <h2 className="text-4xl font-bold mb-8">Our Media Partners</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 md:gap-8">
             {mediaPartners.map((partner) => (
@@ -149,8 +225,8 @@ export default function Component() {
                 <Image
                   src={partner.logo}
                   alt={partner.name}
-                  width={200}
-                  height={200}
+                  width={150}
+                  height={150}
                 />
               </div>
             ))}
@@ -208,7 +284,7 @@ export default function Component() {
               placeholder="Enter your message"
             />
           </div>
-          <Button className="bg-white rounded-none hover:text-primary hover:bg-accent ring-secondary text-2xl font-bold px-10 py-7 ring-[1px]">
+          <Button className="bg-white rounded-none text-secondary hover:text-primary2 hover:bg-white hover:ring-primary2 ring-secondary text-2xl font-bold px-10 py-7 ring-[1px]">
             Send message
           </Button>
         </div>
