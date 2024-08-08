6ba98706dd6d718bd665df1eb098d3edda1fd1d0
kunkoala <azharffrahadian@gmail.com>
2024-08-08 12:58:49 +0200

    updated the event navigaiton bar

--- app/program-and-subject-overview/events/event-navigation.tsx
+++ app/program-and-subject-overview/events/event-navigation.tsx
@@ -21,7 +21,7 @@ export function EventNavigation() {
   });
   return (
     <motion.header
-      className="sticky top-20 bg-white z-51"
+      className="sticky top-20 bg-white shrink-0"
       variants={{
         visible: { y: 0 },
         hidden: { y: "-150%" },
@@ -29,28 +29,30 @@ export function EventNavigation() {
       animate={hidden ? "hidden" : "visible"}
       transition={{ type: "spring", stiffness: 300, damping: 30 }}
     >
-      <Separator className="mb-4" />
-      <div className="flex h-5 items-center justify-center space-x-4 text-sm gap-2">
-        <Link href={PATHS.DAY_ONE}>
-          <div className="hover:bg-gray-100 rounded-lg transition hover:text-accent font-semibold p-2">
-            Day 1
-          </div>
-        </Link>
+      <div className="z-50">
+        <Separator className="mb-4" />
+        <div className="flex h-5 items-center justify-center space-x-4 text-sm gap-2 z-10">
+          <Link href={PATHS.DAY_ONE}>
+            <div className="hover:bg-gray-100 rounded-lg transition hover:text-accent font-semibold p-2">
+              Day 1
+            </div>
+          </Link>
 
-        <Separator orientation="vertical" />
-        <Link href={PATHS.DAY_TWO}>
-          <div className="hover:bg-gray-100 rounded-lg transition hover:text-accent font-semibold p-2">
-            Day 2
-          </div>
-        </Link>
-        <Separator orientation="vertical" />
-        <Link href={PATHS.DAY_THREE}>
-          <div className="hover:bg-gray-100 rounded-lg transition hover:text-accent font-semibold p-2">
-            Day 3
-          </div>
-        </Link>
+          <Separator orientation="vertical" />
+          <Link href={PATHS.DAY_TWO}>
+            <div className="hover:bg-gray-100 rounded-lg transition hover:text-accent font-semibold p-2">
+              Day 2
+            </div>
+          </Link>
+          <Separator orientation="vertical" />
+          <Link href={PATHS.DAY_THREE}>
+            <div className="hover:bg-gray-100 rounded-lg transition hover:text-accent font-semibold p-2">
+              Day 3
+            </div>
+          </Link>
+        </div>
+        <Separator className="mt-4" />
       </div>
-      <Separator className="mt-4" />
     </motion.header>
   );
 }
